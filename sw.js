b2e51c034bd37ca3631481be49cdca959f761b72
colonnam <colonna_m15@yahoo.com>
2021-09-23 16:28:43 +0200

    9. Background Synchronisation - résultat

--- sw.js
+++ sw.js
@@ -1,10 +1,58 @@
-const cacheName = 'veille-techno' + '1.2';
+const cacheName = 'veille-techno' + '1.3';	
+self.importScripts('idb/idb.js', 'idb/database.js');
+
+// 9.6 Synchroniser les données au retour de la connexion
+self.addEventListener('sync', event => {
+    console.log('sync event', event);
+    // test du tag de synchronisation utilisé dans add_techno
+    if (event.tag === 'sync-technos') {
+        console.log('syncing', event.tag);
+        // Utilisation de waitUntil pour s'assurer que le code est exécuté (Attend une promise)
+        event.waitUntil(updateTechnoPromise);
+    }
+})
+ 
+// 9.6 Synchroniser les données au retour de la connexion
+// constante de la Promise permettant de faire la synchronisation
+const updateTechnoPromise = new Promise(function(resolve, reject) {
+ 
+    // récupération de la liste des technos de indexedDB
+    getAllTechnos().then(technos => {
+        console.log('got technos from sync callback', technos);
+        
+        // pour chaque item : appel de l'api pour l'ajouter à la base
+        technos.map(techno => {
+            console.log('Attempting fetch', techno);
+            fetch('https://us-central1-pwa-technos-eb65a.cloudfunctions.net/addTechno', {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify(techno)
+            })
+            .then(() => {
+                // Succès : suppression de l'item en local si ajouté en distant
+                console.log('Success update et id supprimée', techno.id);
+                return deleteTechno(techno.id);
+            })
+            .catch(err => {
+                // Erreur
+                console.log('Error update et id supprimée', err);
+                resolve(err);
+            })
+        })
+ 
+    })
+});
 
 self.addEventListener('install', (evt) => {
     console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
 
     const cachePromise = caches.open(cacheName).then(cache => {
         return cache.addAll([
+            'idb/idb.js',
+            'idb/database.js',
             'index.html',
             'main.js',
             'style.css',
@@ -39,6 +87,14 @@ self.addEventListener('activate', (evt) => {
 });
 
 self.addEventListener('fetch', (evt) => {
+
+     // 9.6 Synchroniser les données au retour de la connexion
+    // console.log('evt', evt);
+    // to prevent this error when posting a form: 
+    // "Uncaught (in promise) TypeError: Request method 'POST' is unsupported at caches.open.then.cache"
+    if(evt.request.method === 'POST') {
+        return;
+    }
 
     // 5.3 Stratégie de network first with cache fallback
     // On doit envoyer une réponse
@@ -148,3 +204,5 @@ self.addEventListener("push", evt => {
     };
     self.registration.showNotification(title, objNotification);
 })
+
+	
